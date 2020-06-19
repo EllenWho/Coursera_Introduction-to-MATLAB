8c8eb2796b25ea6ede0cdf11d5de4cdd492eb3d0
EllenWho <65963047+EllenWho@users.noreply.github.com>
2020-06-19 13:38:41 +0800

    Update saddle.m

--- saddle.m
+++ saddle.m
@@ -1,20 +1,29 @@
 function indices = saddle(M)
 [m, n] = size(M);
-indices = zeros(m*n, 2); %Preallocate the indices(assuming all the numbers are the same in the matrix)
+temp = zeros(m*n, 2); %Preallocate the indices(assuming all the numbers are the same in the matrix)
 %Finding the maximum in the row (might return more than one indexs)
 for ii = 1:m
-    maxn_r = max(M(ii,:)); %maxinum of the row
-    maxi_r = find(M(ii,:) == maxn_r); %indices of the maximum
-    l = length(maxi_r); %number of maximum in the maxi_r
+    maxn = max(M(ii,:)); %maxinum of the row
+    maxi = find(M(ii,:) == maxn); %indices of the maximum
+    l = length(maxi); %number of maximum in the maxi_r
     for jj = 1:l
-        minn_c = min(M(:,maxi_r(jj)));
-        mini_c = find(M(:,maxi_r(jj)) == minn_c);
-        if isequal(maxn_r, minn_c)
-            indices(ii, maxi_r(jj)) = [mini_c, maxi_r];%part of the indices
-        else
-            indices(ii, maxi_r(jj)) = [0, 0];
+        minn = min(M(:,maxi(jj)));
+        mini = find(M(:,maxi(jj)) == minn);
+        if isequal(maxn, minn)
+            for c = 1:length(maxi)
+                for r = 1:length(mini)
+                    temp((ii-1)*m + maxi(c), :) = [mini(r), maxi(c)];%part of the indices
+                end
+            end
         end
     end
+end
+%delete the zeros in the matrix
+if all(temp == 0)
+    indices = [];
+else
+    v = nonzeros(temp');
+    indices = reshape(v,2,2)';
 end
         
 
