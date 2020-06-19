a3a3ac5916a7e072f296c92608bb238e6937fa9a
EllenWho <65963047+EllenWho@users.noreply.github.com>
2020-06-19 19:55:23 +0800

    Update saddle.m
    add some description to the function

--- saddle.m
+++ saddle.m
@@ -1,15 +1,21 @@
+% Defination of saddle point:
+% equal or greater than any element in its row
+% equal or smaller than any elemnet in its column
+% indices has wo columns: col1(row index) col2(column index)
 function indices = saddle(M)
 [m, n] = size(M);
+% Preallocate indices
 temp = zeros(m*n, 2);
 for r = 1:m
     for c = 1:n
-        if M(r,c) >= max(M(r,:)) && M(r,c) <= min(M(:,c))
+        if M(r,c) >= max(M(r,:)) && M(r,c) <= min(M(:,c))  %check every elemnt in this matrix to see if they met the requirement
             temp((r-1)*n+c, :) = [r, c];
         else
             temp((r-1)*n+c, :) = [0,0];
         end
     end
 end
+%remove zeros from the temp, then reshape the matrix
 if all(temp == 0)
     indices = [];
 else
